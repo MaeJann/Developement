eb0579dfabfab4db956c21e5188c4354ad7458a0
Jannik Märsch <Jannik Mrsch>
2017-05-15 14:31:14 +0200

    Changed function to visualize purity map so that for each label class, observations are sorted according the clustering results

--- Unsupervised Clustering.py
+++ Unsupervised Clustering.py
@@ -24,16 +24,29 @@ X = scale(X)  # scale features
 
 #%% Define functions to evaluate and visualize clustering results
 
-def purity_map(predictions):
-# Plotting Clustering results ("Purity Map")
-
-    predictions = predictions.reshape((50,42)) # reshape results((50,42) due to 5 classes with 420 samples each)
+def purity_map(predictions, label_classes, n_samples):
+# Plotting Clustering results ("Purity Map"):
+    
+    
+    # Sort clustering results for each label class i:
+    for i in range(label_classes):
+        predictions [i*n_samples: i*n_samples + n_samples+1] = np.sort(predictions [i*n_samples: i*n_samples + n_samples+1])
+    
+    # Reshape it to map format:
+    new_shape = (label_classes * 10, n_samples/10) # = (50,42) due to 5 classes with 420 samples each)
+    predictions = predictions.reshape(new_shape) 
+    
+    
+    # Visualization:
     plt.imshow(predictions)
     plt.colorbar()
     figure = plt.gca()
     figure.axes.get_xaxis().set_visible(False)
     figure.axes.get_yaxis().set_visible(False)
+    plt.savefig('purity.jpg')
+    plt.show()
 
+    
 
 def purity_metric(predictions, labels, n_cluster):
     class_corr = []
@@ -58,20 +71,22 @@ def purity_metric(predictions, labels, n_cluster):
 
 #%% Create simple GMM and KM Clusterers and apply to data       
 
-clf_GMM = GaussianMixture(n_components = 6)
-clf_GMM.fit(X)
-GMM_results = clf_GMM.predict(X)
-
-GMM_purity, GMM_missed = purity_metric(GMM_results, labels,6 )
-print(GMM_purity)
-print(GMM_missed)
-purity_map(GMM_results)
-
 clf_KM = KMeans(n_clusters = 6)
 clf_KM.fit(X)
 KM_results = clf_KM.predict(X)
 
-KM_purity, KM_missed = purity_metric(KM_results, labels,6 )
-print(KM_purity)
-print(KM_missed)
+KM_purity, KM_missed = purity_metric(KM_results, labels, 6)
+print('KMeans Purity:',KM_purity)
+print('No. of false classifications',KM_missed)
+purity_map(KM_results, 5, 420)
+
+
+clf_GMM = GaussianMixture(n_components = 6)
+clf_GMM.fit(X)
+GMM_results = clf_GMM.predict(X)
+
+GMM_purity, GMM_missed = purity_metric(GMM_results, labels,6)
+print('GMM Purity:', GMM_purity)
+print('No. of false classifications', GMM_missed)
+purity_map(GMM_results, 5, 420)
 
